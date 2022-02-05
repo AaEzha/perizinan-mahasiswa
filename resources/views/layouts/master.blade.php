5f384260ed7d84fa86bdeafd3c16daf253a199a2
AaEzha <aaezha@gmail.com>
2022-02-05 07:06:35 +0700

    disable search result

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -30,7 +30,7 @@
             <input class="form-control" type="search" placeholder="Search" aria-label="Search" data-width="250">
             <button class="btn" type="submit"><i class="fas fa-search"></i></button>
             <div class="search-backdrop"></div>
-            <div class="search-result">
+            {{-- <div class="search-result">
               <div class="search-header">
                 Histories
               </div>
@@ -86,7 +86,7 @@
                   Create a new Homepage Design
                 </a>
               </div>
-            </div>
+            </div> --}}
           </div>
         </form>
         <ul class="navbar-nav navbar-right">
