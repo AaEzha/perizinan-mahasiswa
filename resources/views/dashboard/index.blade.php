e65aa4f18ab5e40b0e484bb0d5c420e0c20919a3
AaEzha <aaezha@gmail.com>
2022-02-05 10:48:44 +0700

    edit: dashboard content

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.master')
 
 @section('content')
-    <h5>This is the dashbaord content.</h5>
+    <h2 class="section-title">Dashbaord content</h2>
+    <p class="section-lead">
+        File ini terletak di <code>dashboard/index.blade.php</code> dan dikontrol oleh controller <code>DashboardController.php</code>.
+    </p>
 @endsection
+
+@push('css')
+    <!--- Contoh Stylesheet --->
+@endpush
+
+@push('js')
+    <!--- Contoh Javascript --->
+@endpush
